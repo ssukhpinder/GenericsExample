27cb2aae61bb0f161e72d0e7bf82823419537391
Sukhpinder Singh <sukhpinder.singh@sourcefuse.com>
2020-08-05 19:43:42 +0530

    added example of generics

--- GenericsExample/Program.cs
+++ GenericsExample/Program.cs
@@ -6,10 +6,38 @@ using System.Threading.Tasks;
 
 namespace GenericsExample
 {
+    class TestGenericClass<T>
+    {
+        //Generic with variable
+        private T variable1;
+
+        //Generic with constructor
+        public TestGenericClass(T value)
+        {
+            variable1 = value;
+        }
+
+        //Generic with member function
+        public T TestFunction(T param1)
+        {
+            Console.WriteLine("Parameter type: {0}, value: {1}", typeof(T).ToString(), param1);
+            Console.WriteLine("Return type: {0}, value: {1}", typeof(T).ToString(), variable1);
+
+            return variable1;
+        }
+
+        //Generic with property
+        public T property1 { get; set; }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            TestGenericClass<int> intGenericClass = new TestGenericClass<int>(10);
+            int val = intGenericClass.TestFunction(200);
+
+            TestGenericClass<string> stringGenericClass = new TestGenericClass<string>("Hello generic");           
+            string val1 = stringGenericClass.TestFunction("Using generic");
         }
     }
 }
